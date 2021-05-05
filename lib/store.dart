dc9e5301c1a06f738c3ba1aad8d501a0cb7a0836
qhwhehwhHans <qhwhehwh@gmail.com>
2021-05-05 23:05:25 +0900

    update json to store

--- lib/store.dart
+++ lib/store.dart
@@ -9,4 +9,22 @@ class Store{
   final int visible;
 
   Store(this.id, this.name, this.app, this.category, this.discount, this.minimunPrice, this.content, this.visible);
+
+  factory Store.fromJson(Map<String, dynamic> json) {
+    return Store(
+      json['id'] as int,
+      json['name'] as String,
+      json['app'] as String,
+      json['category'] as String,
+      json['discount'] as int,
+      json['minimunPrice'] as int,
+      json['content'] as String,
+      json['visible'] as int,
+    );
+  }
+
+  @override
+  String toString() {
+    return "($id, $name, $app, $category, $discount, $minimunPrice, $content, $visible)";
+  }
 }
--- lib/store_manager.dart
+++ lib/store_manager.dart
@@ -2,12 +2,14 @@ import 'package:delivery_coupon_app/store.dart';
 import 'dart:convert';
 
 class StoreManager {
-  List<Store> stores;
+  List<Store> stores = List<Store>.empty();
 
   jsonToStore(String json) {
-    print(json);
-    //Map<String, dynamic> temp = jsonDecode(json);
-    //print(temp);
+    final parsed = jsonDecode(json).cast<Map<String,dynamic>>();
+    stores = parsed.map<Store>((json) => Store.fromJson(json)).toList();
+    for(int i = 0; i < stores.length; ++i) {
+      print(stores[i]);
+    }
   }
 }
 
